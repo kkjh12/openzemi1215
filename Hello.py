8e94c8d93cb2de675f876f0d39d449ef6e9e416a
kkjh12 <64581140+kkjh12@users.noreply.github.com>
2023-12-15 10:14:55 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,21 +1,17 @@
 import streamlit as st
 import base64
-import databutton as db
-
 from openai import OpenAI
 
 # Function to encode the image to base64
 def encode_image(image_file):
     return base64.b64encode(image_file.getvalue()).decode("utf-8")
 
-
 st.set_page_config(page_title="Stock Price Analyst", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
 st.title("📈 Stock Price Analyst: `GPT-4 Turbo with Vision` 👀")
 
-
-# Retrieve the OpenAI API Key from secrets
-api_key = db.secrets.get(name="OPENAI_API_KEY")
+# Retrieve the OpenAI API Key
+api_key = "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"
 
 # Initialize the OpenAI client with the API key
 client = OpenAI(api_key=api_key)
